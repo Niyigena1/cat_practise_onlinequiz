4202a5bae5aeadc81b5ced91d8aba6a2782bb691
Niyigena1 <niyigena1@gmail.com>
2025-12-10 15:39:41 +0000

    fix: Configure Jest to generate JUnit XML test reports for dorny/test-reporter

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   coveragePathIgnorePatterns: ['/node_modules/'],
   collectCoverageFrom: ['**/*.js', '!node_modules/**', '!coverage/**'],
   testMatch: ['**/__tests__/**/*.test.js', '**/?(*.)+(spec|test).js'],
+  testResultsProcessor: 'jest-junit',
   coverageThreshold: {
     global: {
       branches: 70,
